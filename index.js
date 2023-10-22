c17e0f08f5c53e98dc899d0e60751a48bb508466
Roshan <shahroshan0720@gmail.com>
2023-10-22 10:21:42 +0530

    setup get rocket route

--- index.js
+++ index.js
@@ -9,14 +9,6 @@ app.use(responseTime());
 
 const client = redis.createClient();
 
-/*
-(async () => {
-    await client.connect();
-})();
-
-client.on('connect', () => console.log('Redis Client Connected'));
-client.on('error', (err) => console.log('Redis Client Connection Error', err));*/
-
 const GET_ASYNC = promisify(client.get).bind(client);
 const SET_ASYNC = promisify(client.set).bind(client);
 
@@ -32,6 +24,28 @@ app.get('/rockets', async (req, res, next) => {
         const response = await axios.get('https://api.spacexdata.com/v3/rockets');
 
         const saveResult = await SET_ASYNC('rockets', JSON.stringify(response.data), 'EX', 5);
+        console.log('new data cached', saveResult);
+
+        res.send(response.data);
+    } catch (error) {
+        res.send(error.message);
+    }
+})
+
+app.get('/rockets/:rocket_id', async (req, res, next) => {
+    try {
+        const {rocket_id} = req.params;
+        
+        const reply = await GET_ASYNC(rocket_id);
+
+        if (reply) {
+            console.log('using cached data');
+            res.send(JSON.parse(reply));
+            return;
+        }
+        const response = await axios.get(`https://api.spacexdata.com/v3/rockets/${rocket_id}`);
+
+        const saveResult = await SET_ASYNC(rocket_id, JSON.stringify(response.data), 'EX', 5);
         console.log('new data cached', saveResult);
 
         res.send(response.data);
